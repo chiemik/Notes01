3e9066bdf0d317f5c88a4d210061c08185b8d803
Chiemi <ckashimura@mail.valenciacollege.edu>
2016-09-12 16:03:28 +0000

    Notes01

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+    
+    <head>
+        <title>
+            my web page
+        </title>
+    </head>
+    
+    <body>
+        <h1>My Heading</h1>
+        <p>setnence</p>
+        
+        <h6>small Heading</h6>
+        <p>This is a paragraph. </p>
+        <p><em>ITALIC</em> tag is em</p>
+        <p><strong>strong</strong> tag is used to bold text. </p>
+        
+        <?php
+        echo("Hello world php");
+        ?>
+        
+        <script type="text/javascript">
+            document.write("Hello world javascript");
+        </script>
+        
+    </body>
+    
+</html>
